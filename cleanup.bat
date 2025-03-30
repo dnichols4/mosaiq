dc8bdcc0fc7120b688a7a029461f0f894d3b5dbe
Doug Nichols <dnichols4@gmail.com>
2025-03-29 20:23:43 -0500

    cleaned up directories

---
+++ cleanup.bat
@@ -0,0 +1,4 @@
+@echo off
+rmdir /s /q "D:\Workspace\mosaiq\packages\desktop-app\src\renderer\pages\ContentLibraryPage"
+rmdir /s /q "D:\Workspace\mosaiq\packages\desktop-app\src\renderer\pages\LibraryPage"
+echo Directories removed successfully!
