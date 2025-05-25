b3dec774a73bd0aa0447aea324bad20a34ace7c5
Doug Nichols <dnichols4@gmail.com>
2025-05-25 10:19:43 -0500

    cleanup + todo

--- cleanup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rmdir /s /q "D:\Workspace\mosaiq\packages\desktop-app\src\renderer\pages\ContentLibraryPage"
-rmdir /s /q "D:\Workspace\mosaiq\packages\desktop-app\src\renderer\pages\LibraryPage"
-echo Directories removed successfully!
---
+++ todo.md
@@ -0,0 +1,2 @@
+1.  Look at adding concepts for Sports/Football, robotics, etc. based on recent examples
+2.  Update the classification debugger so it outputs the "cleaned" text
